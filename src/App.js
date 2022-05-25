836a09f5d0ca95445b30e54007f860ab067a27e4
abi wardani <abiwardani@abis-MacBook-Pro.local>
2022-05-25 14:58:22 +0700

    multiple component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import './App.css';
-
+import Navbar from './Navbar';
+import Home from './Home';
 function App() {
-  const title = "halo selamat belajar react";
-  const likes = 50;
-  const link = "https://www.google.com";
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1> {title} </h1>
-        <p>Likes = {likes}</p>
-        <p>{10}</p>
-        <p>{[1,2,3,4,]}</p>
-        <p>{Math.random() * 2}</p>
-        <a href={link}>Google Site</a>
+        <Home />
       </div>
     </div>
   );
 }
 
 export default App;
-//dynamic value {}
