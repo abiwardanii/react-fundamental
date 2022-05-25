7de402298797ff1e254ba41e952784135a568260
abi wardani <abiwardani@abis-MacBook-Pro.local>
2022-05-25 15:15:57 +0700

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,24 @@
 const Home = () => {
-    return ( 
-        <div className="home">
-            <h2>HomePage</h2>
-        </div>
-    );
-}
- 
+  const handleClick = () => {
+    console.log("clicked");
+  };
+
+  const handleCLickAgain = (name) => {
+    console.log("Halo " + name);
+  };
+  return (
+    <div className="home">
+      <h2>HomePage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button
+        onClick={() => {
+          handleCLickAgain("abi");
+        }}
+      >
+        Click again
+      </button>
+    </div>
+  );
+};
+
 export default Home;
