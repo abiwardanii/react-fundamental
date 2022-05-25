1d6a1b6f8216d8cee46dd98f2dbc242919229b42
abi wardani <abiwardani@abis-MacBook-Pro.local>
2022-05-25 15:17:49 +0700

    add events and target

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,18 @@
 const Home = () => {
-  const handleClick = () => {
-    console.log("clicked");
+  const handleClick = (e) => {
+    console.log("clicked",e);
   };
 
-  const handleCLickAgain = (name) => {
-    console.log("Halo " + name);
+  const handleCLickAgain = (name,e) => {
+    console.log("Halo " + name, e.target);
   };
   return (
     <div className="home">
       <h2>HomePage</h2>
       <button onClick={handleClick}>Click me</button>
       <button
-        onClick={() => {
-          handleCLickAgain("abi");
+        onClick={(e) => {
+          handleCLickAgain("abi",e);
         }}
       >
         Click again
