2ab5fdb4a142e5db6431d47fe754ccbef7da36c9
abi wardani <abiwardani@abis-MacBook-Pro.local>
2022-05-26 19:38:30 +0700

    Props ienter the commit message for your changes. Lines starting

--- src/BlogsList.js
+++ src/BlogsList.js
@@ -1,8 +1,6 @@
-const BlogList = (props) => {
-    const blogs = props.blogs;
-    const title = props.title;
-
-    console.log(props,blogs);
+const BlogList = ({ blogs, title }) => {
+    // const blogs = props.blogs;
+    // const title = props.title;
   return (
     <div className="blog-list">
         <h2>{title}</h2>
