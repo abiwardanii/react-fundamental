79a4b127f0a8fdfd3804f553696e01d0cfecfbc0
abi wardani <abiwardani@abis-MacBook-Pro.local>
2022-05-26 20:20:38 +0700

    update

--- src/Home.js
+++ src/Home.js
@@ -2,15 +2,20 @@ import { useState } from "react";
 import BlogList from "./BlogsList";
 
 const Home = () => {
-    const [blogs] = useState([
+    const [blogs, setBlogs] = useState([
         { title: 'Ada apa dengan abi', body: 'lorem ipsum...', author: 'abi', id: 1 },
         { title: 'Ada apa dengan tom', body: 'lorem ipsum...', author: 'tom', id: 2 },
         { title: 'Ada apa dengan edo', body: 'lorem ipsum...', author: 'edo', id: 3 }
     ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs);
+    }
+
   return (
     <div className="home">
-        <BlogList blogs={blogs} title="Judul Buku"/>
-        <BlogList blogs={blogs.filter((blog) => blog.author === 'abi')} title="Blog abi"/>
+        <BlogList blogs={blogs} title="Judul Buku" handleDelete={handleDelete}/>
     </div>
   );
 };
